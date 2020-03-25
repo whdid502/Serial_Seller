a2f89b266b4d6a57ac8fbbda3b3868b9e13d67dd
whdid502 <whdid1234@naver.com>
2020-03-25 19:23:18 +0900

    mysql 문자세팅수정, 테스트 db생성

--- mysql_test1.py
+++ mysql_test1.py
@@ -1,7 +1,18 @@
 import pymysql
 
-conn = pymysql.connect(host='220.71.235.155', user='root', password='Whdid7738@', db='serial_seller', charset='utf8')
-curs = conn.cursor()
-sql = "select * from table_A"
-curs.execute(sql)
-data = curs.fetchall()
+conn = pymysql.connect(host='13.209.3.126', user='root', password='Whdid7738@', db='test1', charset='utf8')
+curs = conn.cursor(pymysql.cursors.DictCursor)
+
+
+try:
+    cursor = conn.cursor()
+    name = 'doom'
+    for num in range(10, 20):
+        sql = "INSERT INTO korea (id, name, model_num, model_type) VALUES(" + str(num) + ", " + name + ", '7700', 'Kaby Lake')"
+        print(sql)
+        cursor.execute(sql)
+
+    conn.commit()
+    print(cursor.lastrowid)
+finally:
+    conn.close()
