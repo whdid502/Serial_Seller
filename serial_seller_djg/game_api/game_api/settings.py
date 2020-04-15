9d0cf154b7c52ee52244f1defbf8097066ef0adb
whdid502 <whdid1234@naver.com>
2020-04-15 16:26:59 +0900

    game_api 실행완료, nohup으로 데몬화, 변경된 api url에 맞추어 수정

--- serial_seller_djg/game_api/game_api/settings.py
+++ serial_seller_djg/game_api/game_api/settings.py
@@ -128,5 +128,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8
+    'PAGE_SIZE': 16
 }
