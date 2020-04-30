7497270ccecb4edbd0a55e22fb21a3815ea3b823
whdid502 <whdid1234@naver.com>
2020-04-30 16:37:41 +0900

    로그인&로그아웃 시도, 에러

--- serial_seller_djg/django_login/views.py
+++ serial_seller_djg/django_login/views.py
@@ -1,23 +1,18 @@
-from django.shortcuts import render
-from .forms import SigninForm, SignupForm
-from django.http.response import HttpResponseRedirect
-from django.urls.base import reverse
-from django.contrib.auth.models import User
-
-
-def signup(request):
+from django.contrib.auth import login, authenticate
+def signin(request):
     if request.method == "GET":
-        return render(request, 'customlogin/signup.html', {'f': SignupForm()})
+        return render(request, 'customlogin/signin.html', {'f':SigninForm()} )
     elif request.method == "POST":
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            if form.cleaned_data['password'] == form.cleaned_data['password_check']:
-                new_user = User.objects.create_user(form.cleaned_data['username'], form.cleaned_data['email'], form.cleaned_data['password'])
-                new_user.last_name = form.cleaned_data['last_name']
-                new_user.first_name = form.cleaned_data['first_name']
-                new_user.save()
-                return HttpResponseRedirect(reverse('vote:index'))
-            else:
-                return render(request, 'customlogin/signup.html', {'f': form, 'error': '비밀번호와 비밀번호 확인이 다릅니다.'})
+        form = SigninForm(request.POST)
+        id = request.POST['username']
+        pw = request.POST['password']
+        u = authenticate(username=id, password=pw)
+        if u:
+            login(request, user=u)
+            return HttpResponseRedirect(reverse('vote:index'))
         else:
-            return render(request, 'customlogin/signup.html', {'f': form})
+            return render(request, 'customlogin/signin.html',{'f':form, 'error':'아이디나 비밀번호가 일치하지 않습니다.'})
+from django.contrib.auth import logout
+def signout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('vote:index'))
